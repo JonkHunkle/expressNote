335704a3f369c7e1a527ed70c6dce0c7c4a65b80
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2021-12-04 09:49:23 -0800

    completed routes

--- server.js
+++ server.js
@@ -1,60 +1,103 @@
-const uniqid = require('uniqid')
+//--------boiler
+
+const uniqid = require('uniqid');
 const fs = require('fs')
 const db = require('./db/db.json')
 const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.port || 3001
+const jsonFilePath = path.join(__dirname, "./db/db.json")
 
+//-------
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+
+
+
 app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/db/db.json'))
-})
+  console.info(`${req.method} request received for tips`);
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    console.log('data', data)
+    res.json(JSON.parse(data))
+  });
+});
 
 app.post('/api/notes', (req, res) => {
-  let newNote = req.body
-  let noteID = uniqid()
-  newNote.id = noteID
-  fs.readFile('./db/db.json', (err, data) => {
+
+  const { title, text } = req.body
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uniqid()
+    }
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response)
+    fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+      console.log('new note in read file', newNote)
+      if (err)
+        throw err;
+      const parsedDb = JSON.parse(data)
+
+      parsedDb.unshift(newNote)
+      console.log('parsed data?', parsedDb)
+
+      //console.log('new note array string', noteArray)
+
+      fs.writeFile(jsonFilePath, JSON.stringify(parsedDb), (err) => {
+
+        if (err) throw err;
+        console.log('new note!')
+        res.json(parsedDb)
+
+      })
+    })
+  }
+
+})
+
+
+
+app.delete(`/api/notes/:id`, (req, res) => {
+  fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     if (err)
       throw err;
-    let noteDB = JSON.parse(data)
-    noteDB.push(newNote)
-
-    fs.writeFile('./db/db.json', JSON.stringify(noteDB), 'utf-8', (err, data) => {
+    var parsedData = JSON.parse(data)
+    console.log('delete id', req.params.id)
+    let jsonPath = path.join(__dirname, './db/db.json')
+    dbArray = []
+    for (let i = 0; i < parsedData.length; i++) {
+      if (parsedData[i].id !== req.params.id) {
+        dbArray.push(parsedData[i])
+      }
+    }
+    console.log('new db', dbArray)
+    // res.json(dbArray)
+    fs.writeFile(jsonPath, JSON.stringify(dbArray), (err, data) => {
+      res.json(dbArray)
       if (err) throw err;
-      console.log('new note!')
+      console.log('deleted! note!')
     })
   })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  console.log('delete id', req.params.id)
-  let jsonPath = path.join(__dirname, './db/db.json')
-  for (let i = 0; i < db.length; i++) {
-    if (db[i].id === req.params.id) {
-      db.splice(i, 1)
-      break
-    }
-  }
-  fs.writeFile(jsonPath, JSON.parse(db), (err, data) => {
-    if (err) throw err;
-    console.log('deleted! note!')
-  })
-})
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`))
